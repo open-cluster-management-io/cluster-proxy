f063f8734587774db0149ed29566d29be1ad9c9e
Min Kim <291271447@qq.com>
2022-01-27 15:06:53 +0800

    Revert "Update go version to 1.17 (#58)"
    This reverts commit 9d3fdf8264cb6d9c975ca22b31e8450604565760.

--- vendor/open-cluster-management.io/addon-framework/pkg/addonmanager/constants/constants.go
+++
@@ -1,9 +0,0 @@
-package constants
-
-const (
-	// AddonLabel is the label for addon
-	AddonLabel = "open-cluster-management.io/addon-name"
-
-	// ClusterLabel is the label for cluster
-	ClusterLabel = "open-cluster-management.io/cluster-name"
-)
