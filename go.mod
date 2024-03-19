6f73d11e501a9bfe3e0920c4a63f7fb1d2d3d1be
xuezhao <zxue@redhat.com>
2024-03-19 08:01:11 +0000

    Update stolostronagent.
    Signed-off-by: GitHub <noreply@github.com>

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/onsi/gomega v1.31.1
 	github.com/openshift/library-go v0.0.0-20240304201338-a2ff756d8fc2
 	github.com/pkg/errors v0.9.1
+	github.com/stolostron/cluster-lifecycle-api v0.0.0-20240130112703-747158a63f05
 	github.com/stretchr/testify v1.9.0
 	google.golang.org/grpc v1.62.0
 	k8s.io/api v0.29.2
--- pkg/proxyagent/stolostronagent/agent.go
+++ pkg/proxyagent/stolostronagent/agent.go
@@ -2,6 +2,7 @@ package agent
 
 import (
 	"context"
+	"crypto/sha256"
 	"embed"
 	"encoding/base64"
 	"encoding/json"
@@ -33,6 +34,7 @@ import (
 	"open-cluster-management.io/cluster-proxy/pkg/config"
 	"open-cluster-management.io/cluster-proxy/pkg/proxyserver/operator/authentication/selfsigned"
 	"open-cluster-management.io/cluster-proxy/pkg/util"
+	clustersdkv1beta2 "open-cluster-management.io/sdk-go/pkg/apis/cluster/v1beta2"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -77,6 +79,8 @@ func NewAgentAddon(
 		},
 	}
 	// Register the custom signer CSR option if V1 csr is supported
+	// caculate a hash value of signer ca data and add it to the organizationUnits of the subject
+	signerHash := sha256.Sum256(signer.CAData())
 	if v1CSRSupported {
 		regConfigs = append(regConfigs, addonv1alpha1.RegistrationConfig{
 			SignerName: ProxyAgentSignerName,
@@ -84,6 +88,9 @@ func NewAgentAddon(
 				User: common.SubjectUserClusterProxyAgent,
 				Groups: []string{
 					common.SubjectGroupClusterProxy,
+				},
+				OrganizationUnits: []string{
+					fmt.Sprintf("signer-%x", base64.StdEncoding.EncodeToString(signerHash[:])),
 				},
 			},
 		})
@@ -386,7 +393,7 @@ func managedClusterSetsToFilteredMap(managedClusterSets []clusterv1beta2.Managed
 		}
 
 		// only cluseterSet cover current cluster include in the list.
-		selector, err := clusterv1beta2.BuildClusterSelector(&mcs)
+		selector, err := clustersdkv1beta2.BuildClusterSelector(&mcs)
 		if err != nil {
 			return nil, err
 		}
